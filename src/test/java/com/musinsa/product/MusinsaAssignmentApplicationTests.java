c3d45b5f8d6259580593b91d1a541ed36edc9eb3
minhyeok <omh34658981@gmail.com>
2025-03-28 12:41:00 +0900

    initial commit

---
+++ src/test/java/com/musinsa/product/MusinsaAssignmentApplicationTests.java
@@ -0,0 +1,15 @@
+package com.musinsa.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class MusinsaAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
